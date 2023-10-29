bbe3e65ae5869c2de8cdb0faeda1f68d48aace35
gdiego07 <40152772+gdiego07@users.noreply.github.com>
2023-10-28 23:39:50 -0700

    Working on collecting historical weather data

--- scraper/weather_scraper.py
+++ scraper/weather_scraper.py
@@ -2,3 +2,48 @@
 #making it another great example for a Weather-based Stock Analysis App. Analyzing the impact of weather 
 #on Bayer’s agricultural division could provide valuable insights for investors interested in the 
 #intersection of agriculture and financial markets.
+from bs4 import BeautifulSoup
+import requests
+import random
+import os
+import json
+
+def fetch_corn_belt_weather_data():
+    url = [
+        'https://www.weather.gov/wrh/climate?wfo=dmx',
+        'May need to use the API to access historical data',
+    ]
+    referer = 'https://www.weather.gov'
+    user_agents = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/117.0',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'
+    ]
+    headers = {
+        'User-Agent': random.choice(user_agents),
+        'Referer': referer
+    }
+    
+    try:
+        with requests.Session() as session:
+            session.headers.update(headers)
+            response = session.get(url, timeout = 10)
+        response.raise_for_status()
+        
+    #Works with the try block
+    except requests.ConnectionError:
+        print('Failed to connect to website')
+    except requests.Timeout:
+        print('The request timed out')
+    except requests.RequestException as e:
+        print(f'An error occurred while fetching the data: {e}')
+    except Exception as e:
+        print(f'An unexpected error occurred: {e}')
